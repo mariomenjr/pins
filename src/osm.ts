a80a3e94f16cdc83a3e78961208ccba5f13d41df
Mario Menjivar <mariomenjr@gmail.com>
2025-07-30 01:06:50 -0700

    Reconfigure heatmap params and update default location

--- src/osm.ts
+++ src/osm.ts
@@ -9,15 +9,17 @@ import maplibregl, { GeoJSONSource } from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 
 export default class Osm {
-  static readonly ZOOM = 9;
+  static readonly ZOOM = 11;
   static readonly MAX_ZOOM = 21;
   static readonly MIN_ZOOM = 3;
+
+  static readonly HEATMAP_MAX_MAG = 5;
 
   static readonly MAP_DIV = "map";
   static readonly VECTOR_TITLE_URL =
     "https://tiles.openfreemap.org/styles/liberty";
 
-  static readonly FALLBACK_COORDS: maplibregl.LngLatLike = [-0.09, 51.505];
+  static readonly FALLBACK_COORDS: maplibregl.LngLatLike = [-117.6591, 33.5104]; // [-0.09, 51.505];
 
   static map: maplibregl.Map | null = null;
   static geolocate: maplibregl.GeolocateControl | null = null;
@@ -80,7 +82,7 @@ export default class Osm {
           properties: {
             // name: `Random Point ${data.features.length + 1}`,
             id: time,
-            mag: Math.random() * 6,
+            mag: Math.random() * Osm.HEATMAP_MAX_MAG,
             time: time,
             felt: null,
             tsunami: 0,
@@ -107,7 +109,24 @@ export default class Osm {
       type: "geojson",
       data: {
         type: "FeatureCollection",
-        features: [],
+        features: Array.from({ length: 50 }).map((_, i) => ({
+          type: "Feature" as const,
+          geometry: {
+            type: "Point" as const,
+            coordinates: [
+              -117.63695568237284 + 0.1 * Math.random(),
+              33.50267070026682 + 0.1 * Math.random(),
+            ],
+          },
+          properties: {
+            // name: `Random Point ${data.features.length + 1}`,
+            id: Date.now(),
+            mag: Math.random() * Osm.HEATMAP_MAX_MAG,
+            time: Date.now(),
+            felt: null,
+            tsunami: 0,
+          },
+        })),
       },
     });
 
@@ -123,12 +142,20 @@ export default class Osm {
           ["get", "mag"],
           0,
           0,
-          6,
+          Osm.HEATMAP_MAX_MAG,
           1,
         ],
         // Increase the heatmap color weight weight by zoom level
         // heatmap-intensity is a multiplier on top of heatmap-weight
-        "heatmap-intensity": ["interpolate", ["linear"], ["zoom"], 0, 1, 9, 3],
+        "heatmap-intensity": [
+          "interpolate",
+          ["linear"],
+          ["zoom"],
+          Osm.ZOOM,
+          9,
+          13,
+          3,
+        ],
         // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
         // Begin color ramp at 0-stop with a 0-transparency color
         // to create a blur-like effect.
@@ -150,9 +177,17 @@ export default class Osm {
           "rgb(178,24,43)",
         ],
         // Adjust the heatmap radius by zoom level
-        "heatmap-radius": ["interpolate", ["linear"], ["zoom"], 0, 2, 9, 20],
+        "heatmap-radius": ["interpolate", ["linear"], ["zoom"], 1, 1, 13, 24],
         // Transition from heatmap to circle layer by zoom level
-        "heatmap-opacity": ["interpolate", ["linear"], ["zoom"], 7, 1, 9, 0],
+        "heatmap-opacity": [
+          "interpolate",
+          ["linear"],
+          ["zoom"],
+          Osm.ZOOM,
+          1,
+          13,
+          0,
+        ],
       },
     });
 
@@ -160,40 +195,72 @@ export default class Osm {
       id: "sighting-point",
       type: "circle",
       source: "sighting",
-      minzoom: 7,
+      minzoom: Osm.MIN_ZOOM,
       paint: {
         // Size circle radius by earthquake magnitude and zoom level
         "circle-radius": [
           "interpolate",
           ["linear"],
           ["zoom"],
-          7,
-          ["interpolate", ["linear"], ["get", "mag"], 1, 1, 6, 4],
-          16,
-          ["interpolate", ["linear"], ["get", "mag"], 1, 5, 6, 50],
+          1,
+          [
+            "interpolate",
+            ["linear"],
+            ["get", "mag"],
+            1,
+            1,
+            Osm.HEATMAP_MAX_MAG,
+            5,
+          ],
+          13,
+          [
+            "interpolate",
+            ["linear"],
+            ["get", "mag"],
+            1,
+            6,
+            Osm.HEATMAP_MAX_MAG,
+            12,
+          ],
         ],
         // Color circle by earthquake magnitude
         "circle-color": [
           "interpolate",
           ["linear"],
           ["get", "mag"],
-          1,
+          0,
           "rgba(33,102,172,0)",
-          2,
+          1,
           "rgb(103,169,207)",
-          3,
+          2,
           "rgb(209,229,240)",
-          4,
+          3,
           "rgb(253,219,199)",
-          5,
+          4,
           "rgb(239,138,98)",
-          6,
+          5,
           "rgb(178,24,43)",
         ],
         "circle-stroke-color": "white",
-        "circle-stroke-width": 1,
+        "circle-stroke-width": [
+          "interpolate",
+          ["linear"],
+          ["zoom"],
+          11,
+          0,
+          13,
+          2.5,
+        ],
         // Transition from heatmap to circle layer by zoom level
-        "circle-opacity": ["interpolate", ["linear"], ["zoom"], 7, 0, 8, 1],
+        "circle-opacity": [
+          "interpolate",
+          ["linear"],
+          ["zoom"],
+          12,
+          0.5,
+          12.5,
+          1,
+        ],
       },
     });
   }
